e217688cbf65186fdcecc5daf4fabdc5018a308d
soccerlexi13 <soccerlexi13@gmail.com>
2015-09-29 09:14:23 -0700

    edited tree

--- Christmas Tree/ChristmasTree.java
+++ Christmas Tree/ChristmasTree.java
@@ -1,4 +1,3 @@
-
 /**
  * PP 3.1, draws a christmas tree using parameters
  * 
@@ -8,28 +7,38 @@
 public class ChristmasTree
 {
    public static void main(String[] args){
-       drawTree(2, 5);
+       drawTree(6, 8);
    }
    public static void drawTree(int numSegments, int segHeight){
-       int minNumStars, starsInCurrentRow;
-       //int maxNumStars;
-       for (int i=1; i<=numSegments; i++){
-           minNumStars=(2*i)-1;
-           //maxNumStars=minNumStars+2*(segHeight-1);
-           starsInCurrentRow=minNumStars;
-           for(int j=1; j<=segHeight; j++){
-               for (int k=1; k<=6-starsInCurrentRow/2; k++){
-                   System.out.print(" ");
-               }
-               for (int m=1; m<=starsInCurrentRow; m++){
+       int minStarsInSeg, starsInCurrentRow;
+       int maxNumStars=(2*numSegments-1)+2*(segHeight-1);
+       //initialized outside because it never changes
+       for (int currentSeg=1; currentSeg<=numSegments; currentSeg++){
+           minStarsInSeg=(2*currentSeg)-1;
+           starsInCurrentRow=minStarsInSeg;
+           for(int currentRow=1; currentRow<=segHeight; currentRow++){
+               drawSpaces((maxNumStars-starsInCurrentRow)/2); 
+               //spaces rely on the biggest row of stars because everything not that will be spaced
+               for (int i=1; i<=starsInCurrentRow; i++){
                     System.out.print("*");
                }
                System.out.println();
                starsInCurrentRow+=2;
+               //needs to be incremented by two and not one because that's how the tree grows
             }
        }
-       System.out.println("      *");
-       System.out.println("      *");
-       System.out.println("   *******");
+       //below here, prints base
+       for (int i=1; i<=2; i++){
+           drawSpaces(maxNumStars/2);
+           System.out.println("*");
+       }
+       drawSpaces(maxNumStars/2-3);
+       System.out.println("*******");
+   }
+   public static void drawSpaces(int numSpaces){
+       //reduces/simplifies code by making method to draw spaces
+       for (int i=1; i<=numSpaces; i++){
+           System.out.print(" ");
+       }
    }
 }
