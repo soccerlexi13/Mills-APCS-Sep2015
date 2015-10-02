aa25b0b218c6572c27d740413b968ac6e4c3a871
soccerlexi13 <soccerlexi13@gmail.com>
2015-10-02 08:51:55 -0700

    doctor who done? :hourglass:

--- Ascii Art Project/DoctorWho.java
+++ Ascii Art Project/DoctorWho.java
@@ -9,6 +9,8 @@ public class DoctorWho
 {
     public static void main(String[] args){
         drawTardis(3, 2);
+        drawTardis(2, 1);
+        drawTardis(1, 3);
     }
     public static void drawTardis(int windowHeight, int windowRowsAfterFirst){
         //it's windowrowsafterfirst because first is different/necessary
@@ -21,26 +23,34 @@ public class DoctorWho
         drawSingleLineAcross();
         System.out.println("||  POLICE  BOX  ||");
         drawSingleLineAcross();
+        //top of tardis is unique, can't simplify besides spaces
         drawWindowTop();
         for (int i=1; i<=windowHeight; i++){
+            //number of rows in window depends on the parameter windowHeight
             for (int j=1; j<=9; j++){
                 System.out.print(" |");
+                //paned windows have repeated pattern all the way across
             }
             System.out.println();
         }
         drawWindowBottom();
         for (int i=1; i<=windowRowsAfterFirst; i++){
+            //total number of windows after first depends on parameter windowRowsAfterFirst
             drawWindowTop();
             for (int j=1; j<=windowHeight; j++){
+                //middle rows of window repeated as many times as it is supposed to be tall
                 drawWindowMiddle();
             }
             drawWindowBottom();
         }
         for (int i=1; i<=19; i++){
             System.out.print("=");
+            //bottom of tardis
         }
+        System.out.println();
     }
     public static void drawSpaces(int numSpaces){
+        //useful function that prints a specified number of spaces on a single line
         for (int i=1; i<=numSpaces; i++){
             System.out.print(" ");
         }
@@ -49,6 +59,7 @@ public class DoctorWho
         for (int i=1; i<=19; i++){
             System.out.print("-");
         }
+        //for loop to draw lines in top portion, just repeated -
         System.out.println();
     }
     public static void drawWindowTop(){
@@ -56,26 +67,21 @@ public class DoctorWho
         for (int i=1; i<=2; i++){
             System.out.print(" _____ |");
         }
+        //pattern for top of window repeated
         System.out.println();
     }
     public static void drawWindowBottom(){
         for (int i=1; i<=2; i++){
             System.out.print(" | -----");
         }
+        //bottom of window also has repeatable pattern
         System.out.println(" |");
     }
     public static void drawWindowMiddle(){
         for (int i=1; i<=2; i++){
-            System.out.print(" |");
+            System.out.print(" | |   |");
         }
-        System.out.print("  ");
-        for (int i=1; i<=3; i++){
-            System.out.print(" |");
-        }
-        System.out.print("  ");
-        for (int i=1; i<=2; i++){
-            System.out.print(" |");
-        }
-        System.out.println();
+        System.out.println(" |");
+        //middle of windows has repeated parts
     }
 }
